f0d0b788f83392dd48efcda78c4b661bb986179c
Ghost <naclzfaklz@gmail.com>
2023-12-06 20:48:19 +0300

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import { ReactNode } from 'react'
+import BackgroundImage from '../../public/login_background.jpg'
+import Logo from '../../public/netflix_logo.svg'
+
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+	return (
+		<div className='relative flex w-screen h-screen	 flex-col bg-black md:items-center md:justify-center md:bg-transparent'>
+			<Image
+				src={BackgroundImage}
+				alt='background image'
+				className='hidden sm:flex sm:object-cover -z-10 brightness-50'
+				priority
+				fill
+			/>
+
+			<Image
+				src={Logo}
+				alt='logo'
+				width={120}
+				height={120}
+				priority
+				className='absolute left-4 t-4  md:left-10 md:top-6'
+			/>
+
+			{children}
+		</div>
+	)
+}
+
+export default AuthLayout
